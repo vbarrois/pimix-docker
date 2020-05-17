8d5730cf5ea1dc67521c75ccbbfa198e7b2c756d
Vincent Barrois <vbarrois@gmail.com>
2020-05-17 22:09:14 +0200

    <feat> concurrently colors (chalk)

--- startup.sh
+++ startup.sh
@@ -11,4 +11,4 @@ npx sequelize-cli db:seed:all
 /wait
 
 cd /usr/src/pimix
-npx concurrently -c "bgBlue.bold,bgMagenta.bold,bgRed,bgGray" "node pimix-router/router.js" "node pimix-data/data.js" "node pimix-data/scanner.js" "node pimix-ui/server.js"
+npx concurrently -c "bgYellowBright,bgGreenBright,bgRedBright,bgBlueBright" "node pimix-router/router.js" "node pimix-data/data.js" "node pimix-data/scanner.js" "node pimix-ui/server.js"
