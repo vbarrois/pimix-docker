b06f1bfb2a3e67ee01c855a3059236e9089d2a93
Vincent Barrois <vbarrois@gmail.com>
2020-05-25 11:59:34 +0200

    <version> 1.0.8 - Preferences

---
+++ dist/pimix-data/migrations/update-add-artist-state.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.addColumn(
+      'Artists',
+      'state',
+      {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true
+      }
+    );
+
+  },
+
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.removeColumn(
+      'Artists',
+      'state'
+    );
+  }
+}
