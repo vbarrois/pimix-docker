221dd17764330f1b5236112773e74596f7dbf3dc
Vincent Barrois <vbarrois@gmail.com>
2020-06-27 17:42:42 +0200

    <docker> npx sequelize --no-install

--- startup.sh
+++ startup.sh
@@ -5,8 +5,8 @@ redis-server&
 cp -a /usr/src/pimix/pimix-player/. /home/pimix-player
 
 cd /usr/src/pimix/pimix-data
-npx sequelize-cli db:migrate 
-npx sequelize-cli db:seed:all
+npx --no-install sequelize-cli db:migrate 
+npx --no-install sequelize-cli db:seed:all
 
 cd /usr/src/pimix
 npx concurrently -c "bgYellowBright,bgGreenBright,bgRedBright,bgBlueBright" "node pimix-router/router.js" "node pimix-data/data.js" "node pimix-data/scanner.js" "node pimix-ui/server.js"
