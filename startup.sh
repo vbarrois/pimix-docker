61d000029d5e482bad797be1e2ea02cb2bcda6c4
vbarrois@gmail.com <vbarrois@gmail.com>
2023-02-26 09:39:52 +0100

    production

--- startup.sh
+++ startup.sh
@@ -9,5 +9,7 @@ yarn add sequelize-cli
 npx --no-install sequelize-cli db:migrate 
 npx --no-install sequelize-cli db:seed:all
 
+service nginx start
+
 cd /usr/src/pimix
-npx concurrently -c "bgYellowBright,bgGreenBright,bgRedBright,bgWhiteBright,bgBlueBright" "node pimix-router/router.js" "node pimix-data/rest.js" "node pimix-data/scanner.js" "node pimix-data/wifi.js" "node pimix-ui/server.js"
+npx concurrently -c "bgYellowBright,bgGreenBright,bgRedBright,bgWhiteBright,bgBlueBright" "node pimix-router/router.js" "node pimix-data/rest.js" "node pimix-data/scanner.js" "node pimix-data/wifi.js"
