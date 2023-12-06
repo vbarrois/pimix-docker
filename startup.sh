6713b498503e9f72c39a49c52724aa8be0eefc2c
vbarrois@gmail.com <vbarrois@gmail.com>
2023-12-06 13:28:49 +0100

    docker with player included

--- startup.sh
+++ startup.sh
@@ -2,9 +2,6 @@
 
 redis-server&
 
-rm /home/pimix-player/pimix-player.jar
-cp -n -a /usr/src/pimix/pimix-player /home/pimix-player
-
 cd /usr/src/pimix/pimix-data
 yarn add sequelize-cli
 npx --no-install sequelize-cli db:migrate 
@@ -13,4 +10,4 @@ npx --no-install sequelize-cli db:seed:all
 service nginx start
 
 cd /usr/src/pimix
-npx concurrently -c "bgYellowBright,bgGreenBright,bgRedBright,bgWhiteBright,bgBlueBright,bgWhiteBright" "node pimix-data/pimix-cluster.js" "node pimix-data/scanner.js" "node pimix-data/wifi.js" "node pimix-data/AI.js" "node pimix-router/router.js"
+npx concurrently -c "bgYellowBright,bgGreenBright,bgRedBright,bgWhiteBright,bgBlueBright,bgWhiteBright" "node pimix-data/pimix-cluster.js" "node pimix-data/scanner.js" "node pimix-data/wifi.js" "node pimix-data/AI.js" "node pimix-router/router.js" "java -jar /usr/src/pimix/pimix-player/pimix-player.jar"
