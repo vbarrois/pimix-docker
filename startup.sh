6218731bc7f727c66f67c08d3d4990070ab1e1ed
vbarrois@gmail.com <vbarrois@gmail.com>
2023-12-06 14:27:09 +0100

    fix yarn add and start nginx first

--- startup.sh
+++ startup.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
+service nginx start
 
 redis-server&
 
 cd /usr/src/pimix/pimix-data
-yarn add sequelize-cli
 npx --no-install sequelize-cli db:migrate 
 npx --no-install sequelize-cli db:seed:all
 
-service nginx start
-
 cd /usr/src/pimix
-npx concurrently -c "bgYellowBright,bgGreenBright,bgRedBright,bgWhiteBright,bgBlueBright,bgWhiteBright" "node pimix-data/pimix-cluster.js" "node pimix-data/scanner.js" "node pimix-data/wifi.js" "node pimix-data/AI.js" "node pimix-router/router.js" "java -jar /usr/src/pimix/pimix-player/pimix-player.jar"
+npx concurrently -c "bgYellowBright,bgGreenBright,bgRedBright,bgWhiteBright,bgBlueBright,bgWhiteBright" "node pimix-data/pimix-cluster.js" "node pimix-data/scanner.js" "node pimix-data/wifi.js" "node pimix-router/router.js" "java -jar /usr/src/pimix/pimix-player/pimix-player.jar"
